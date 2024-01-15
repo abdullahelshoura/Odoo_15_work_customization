c305933c63f9fa4b711bf0b4ea580a3fb3451266
abdullahelshoura <shourauid@gmail.com>
2024-01-15 14:05:47 +0200

    creating new module that customizes purchase order report base customizations

---
+++ purchase_order_report/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Purchase Report Edit',
+    'summary': 'purchase_report_edit',
+    'version': '0.1',
+    'category': 'purchase_report_edit',
+    'website': 'https://github.com/OCA',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': [
+        'base', 'purchase',
+    ],
+    'data': [
+        'views/purchase_order_inherit.xml',
+    ],
+
+}
